cd7bb8312460f7077679c0b2f62d55ea68261528
Yannstj <yannick.saintjust@gmail.com>
2024-03-18 18:43:15 +0100

    init input control

--- scripts/login.js
+++ scripts/login.js
@@ -1,20 +1,29 @@
-function login() {
+async function login() {
   const formLogin = document.querySelector(".form-login");
   formLogin.addEventListener("submit", (event) => {
     event.preventDefault();
+    const email = event.target.querySelector("[name=email]").value;
+    const password = event.target.querySelector("[name=password]").value;
     const logData = {
-      email: event.target.querySelector("[name=email]").value,
-      password: event.target.querySelector("[name=password]").value,
+      email: email,
+      password: password,
     };
-    // Conversion au format JSON
+    // Conversion du json en string pour le body de la requete
     const chargeUtile = JSON.stringify(logData);
-    //console.log(chargeUtile);
-    fetch("http://localhost:5678/api/users/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: chargeUtile,
-    });
+    // le controle des champs ici est temporaire
+    if (email || password === "") {
+      console.log("champ vide");
+    } else {
+      fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: chargeUtile,
+      });
+    }
   });
 }
 
 login();
+
+// Faire une fonction de validation de l'email et une autre pour le mdp
+// si l'une ET l'autre revoie true alors on fetch l'Api
