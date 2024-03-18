ab24e2a94a569aef74e02237bbc7b48a8776dd57
Yannstj <yannick.saintjust@gmail.com>
2024-03-18 15:47:57 +0100

    filter v1 starting login page

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,7 @@ const response = await fetch("http://localhost:5678/api/works");
 async function fetchWork() {
   if (response.ok) {
     // without return, we log the error to
+    // response.json() return a promise
     return response.json();
   }
   throw new Error("Erreur lors de la récuperation des données");
@@ -13,32 +14,16 @@ async function fetchWork() {
 // Traitements des data reçu
 fetchWork().then((works) => {
   generateGalerie(works);
+  allGalerie(works);
   filterObjets(works);
+  filterAppartement(works);
+  filterHotelAndRestaurant(works);
 });
 // Appel des fonction
 
 generateMenu();
 
 // Creation des fonction
-
-// Affichage dynamique de la gallerie
-function generateGalerie(works) {
-  const gallery = document.querySelector(".gallery");
-  console.log(works);
-  gallery.innerHTML = "";
-  for (let i = 0; i < works.length; i++) {
-    const figure = document.createElement("figure");
-    const img = document.createElement("img");
-    const figcaption = document.createElement("figcaption");
-    img.setAttribute("src", works[i].imageUrl);
-    img.setAttribute("alt", works[i].title);
-    figcaption.innerText = works[i].title;
-
-    gallery.appendChild(figure);
-    figure.appendChild(img);
-    figure.appendChild(figcaption);
-  }
-}
 
 // Affichage dynamique des bouttons
 function generateMenu() {
@@ -69,12 +54,110 @@ function generateMenu() {
   divButton.appendChild(buttonHotelRestaurant);
 }
 
+// Affichage dynamique de la gallerie
+function generateGalerie(works) {
+  const gallery = document.querySelector(".gallery");
+  console.log(works);
+  gallery.innerHTML = "";
+  for (let i = 0; i < works.length; i++) {
+    const figure = document.createElement("figure");
+    const img = document.createElement("img");
+    const figcaption = document.createElement("figcaption");
+    img.setAttribute("src", works[i].imageUrl);
+    img.setAttribute("alt", works[i].title);
+    figcaption.innerText = works[i].title;
+
+    gallery.appendChild(figure);
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+  }
+}
+
+/// FILTRE ///
+
+// Toutes la galerie
+function allGalerie(works) {
+  document.querySelector("#btn-all").addEventListener("click", () => {
+    generateGalerie(works);
+  });
+}
+
+// Filtre Objets
 function filterObjets(works) {
   const btnObj = document.querySelector("#btn-obj");
   btnObj.addEventListener("click", () => {
     const objetsFilter = works.filter(
       (works) => works.category.name === "Objets"
     );
-    console.log(objetsFilter);
+    // Au clique on efface la gallery
+    const gallery = document.querySelector(".gallery");
+    gallery.innerHTML = "";
+    // ON parcours l'array filtrer et crée les nouveaux élements
+    for (let i = 0; i < objetsFilter.length; i++) {
+      const figure = document.createElement("figure");
+      const img = document.createElement("img");
+      const figcaption = document.createElement("figcaption");
+      img.setAttribute("src", objetsFilter[i].imageUrl);
+      img.setAttribute("alt", objetsFilter[i].title);
+      figcaption.innerText = objetsFilter[i].title;
+
+      gallery.appendChild(figure);
+      figure.appendChild(img);
+      figure.appendChild(figcaption);
+    }
+  });
+}
+
+// Filtre appartements
+
+function filterAppartement(works) {
+  const buttonAppart = document.querySelector("#btn-appart");
+  buttonAppart.addEventListener("click", () => {
+    const appartFilter = works.filter(
+      (works) => works.category.name === "Appartements"
+    );
+    // Au clique on efface la gallery
+    const gallery = document.querySelector(".gallery");
+    gallery.innerHTML = "";
+    // ON parcours l'array filtrer et crée les nouveaux élements
+    for (let i = 0; i < appartFilter.length; i++) {
+      const figure = document.createElement("figure");
+      const img = document.createElement("img");
+      const figcaption = document.createElement("figcaption");
+      img.setAttribute("src", appartFilter[i].imageUrl);
+      img.setAttribute("alt", appartFilter[i].title);
+      figcaption.innerText = appartFilter[i].title;
+
+      gallery.appendChild(figure);
+      figure.appendChild(img);
+      figure.appendChild(figcaption);
+    }
+  });
+}
+
+// Filtre Hotel & restaurants
+
+function filterHotelAndRestaurant(works) {
+  const buttonHotelRestaurant = document.querySelector("#btn-hotel-restaurant");
+  buttonHotelRestaurant.addEventListener("click", () => {
+    const hotelAndRestaurantFilter = works.filter(
+      (works) => works.category.name === "Hotels & restaurants"
+    );
+    // Au clique on efface la gallery
+    const gallery = document.querySelector(".gallery");
+    gallery.innerHTML = "";
+    // ON parcours l'array filtrer et crée les nouveaux élements
+    for (let i = 0; i < hotelAndRestaurantFilter.length; i++) {
+      const figure = document.createElement("figure");
+      const img = document.createElement("img");
+      const figcaption = document.createElement("figcaption");
+      img.setAttribute("src", hotelAndRestaurantFilter[i].imageUrl);
+      img.setAttribute("alt", hotelAndRestaurantFilter[i].title);
+      figcaption.innerText = hotelAndRestaurantFilter[i].title;
+
+      gallery.appendChild(figure);
+      figure.appendChild(img);
+      figure.appendChild(figcaption);
+    }
   });
 }
