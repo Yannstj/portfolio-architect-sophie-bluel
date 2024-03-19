368695ce1fe841df56201a5e4e690552323c146e
Yannstj <yannick.saintjust@gmail.com>
2024-03-19 14:21:46 +0100

    refacto try catch for email and password validity

--- scripts/login.js
+++ scripts/login.js
@@ -1,3 +1,7 @@
+// Les logs sont :
+//email: sophie.bluel@test.tld
+//password: S0phie
+
 //Initialisation
 
 // Création des fonctions
@@ -5,47 +9,67 @@ function login() {
   const formLogin = document.querySelector(".form-login");
   formLogin.addEventListener("submit", (event) => {
     event.preventDefault();
-    const email = event.target.querySelector("[name=email]").value;
-    const password = event.target.querySelector("[name=password]").value;
-    const logData = {
-      email: email,
-      password: password,
-    };
-    // Conversion du json en string pour le body de la requete
-    const chargeUtile = JSON.stringify(logData);
-    // le controle des champs ici est temporaire
-    // .value sinon la condition est toujours true
-    // le controle ne marche plus 19/03/24
-    if (email.value || password.value === "") {
-      console.log(email);
-      console.log(password);
-      console.log("champ vide");
-    } else {
-      async function postLoginRequest() {
-        const loginRequestResponse = await fetch(
-          "http://localhost:5678/api/users/login",
-          {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: chargeUtile,
-          }
-        );
-        //console.log(loginRequestResponse);
-        if (loginRequestResponse.ok) {
-          document.location.href = "index.html";
-        }
-      }
-      postLoginRequest();
+    try {
+      let email = event.target.querySelector("[name=email]").value;
+      verfierEmail(email);
+      let password = event.target.querySelector("[name=password]").value;
+      verifierPassword(password);
+      // Set data to JSON
+      let logData = {
+        email: email,
+        password: password,
+      };
+      // Conversion du json en string pour le body de la requete
+      let chargeUtile = JSON.stringify(logData);
+      afficherMessageErreur("");
+      postLoginRequest(chargeUtile);
+    } catch (erreur) {
+      afficherMessageErreur(erreur.message);
     }
   });
 }
+// nb bien mettre le try catch dans l'eventListnner sinon bug
 
 login();
-
-// Faire une fonction de validation de l'email et une autre pour le mdp
 // si l'une ET l'autre revoie true alors on fetch l'Api
 
-// Les logs sont :
-//email: sophie.bluel@test.tld
-//password: S0phie
 // authorizer avant sur l'api ... Si la réponses est ok alors on redirige
+
+function verfierEmail(email) {
+  const emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+.[a-z0-9._-]+");
+  if (!emailRegExp.test(email)) {
+    throw new Error("L'email n'est pas valide");
+  }
+}
+
+function verifierPassword(password) {
+  if (password.length < 5) {
+    throw new Error("Le mot de passe est trop court");
+  }
+}
+
+function afficherMessageErreur(message) {
+  let spanErreurMessage = document.getElementById("erreurMessage");
+  if (!spanErreurMessage) {
+    const form = document.querySelector(".form");
+    spanErreurMessage = document.createElement("span");
+    spanErreurMessage.id = "erreurMessage";
+    form.append(spanErreurMessage);
+  }
+  spanErreurMessage.innerText = message;
+}
+
+async function postLoginRequest(chargeUtile) {
+  const loginRequestResponse = await fetch(
+    "http://localhost:5678/api/users/login",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: chargeUtile,
+    }
+  );
+  //console.log(loginRequestResponse);
+  if (loginRequestResponse.ok) {
+    document.location.href = "index.html";
+  }
+}
