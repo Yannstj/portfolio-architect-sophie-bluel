ce88bc677681aed0a9eefcddd6522db841a3d780
Yannstj <yannick.saintjust@gmail.com>
2024-03-19 15:38:32 +0100

    gestion erreur serveur with switch case

--- scripts/login.js
+++ scripts/login.js
@@ -4,6 +4,9 @@
 
 //Initialisation
 
+//Appel des fonctions
+login();
+
 // Création des fonctions
 function login() {
   const formLogin = document.querySelector(".form-login");
@@ -30,13 +33,22 @@ function login() {
 }
 // nb bien mettre le try catch dans l'eventListnner sinon bug
 
-login();
-// si l'une ET l'autre revoie true alors on fetch l'Api
+async function postLoginRequest(chargeUtile) {
+  const loginRequestResponse = await fetch(
+    "http://localhost:5678/api/users/login",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: chargeUtile,
+    }
+  );
+  gererPostRequest(loginRequestResponse);
+}
 
-// authorizer avant sur l'api ... Si la réponses est ok alors on redirige
-
+// fonctions de gestion de la validité des inputs
 function verfierEmail(email) {
   const emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+.[a-z0-9._-]+");
+  // Si email ne correspond pas
   if (!emailRegExp.test(email)) {
     throw new Error("L'email n'est pas valide");
   }
@@ -59,17 +71,22 @@ function afficherMessageErreur(message) {
   spanErreurMessage.innerText = message;
 }
 
-async function postLoginRequest(chargeUtile) {
-  const loginRequestResponse = await fetch(
-    "http://localhost:5678/api/users/login",
-    {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: chargeUtile,
-    }
-  );
-  //console.log(loginRequestResponse);
-  if (loginRequestResponse.ok) {
-    document.location.href = "index.html";
+// fonctions de gestion de la reponse serveur
+
+function gererPostRequest(response) {
+  // switch va tester si la valeur passer en paramètre === la valeur de case
+  // on a donc ici res.status = x (number) === x (number)
+  switch (response.status) {
+    case 200:
+      document.location.href = "index.html";
+      break;
+    case 401:
+      console.log("erreur 401");
+      break;
+    case 404:
+      console.log("erreur 404");
+      break;
+    default:
+      console.log("erreur interne");
   }
 }
