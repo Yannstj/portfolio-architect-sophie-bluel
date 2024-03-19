3f6be02e2ec314381920aa19f0971599be555783
Yannstj <yannick.saintjust@gmail.com>
2024-03-19 01:56:22 +0100

    post response ok

--- scripts/login.js
+++ scripts/login.js
@@ -1,4 +1,7 @@
-async function login() {
+//Initialisation
+
+// Création des fonctions
+function login() {
   const formLogin = document.querySelector(".form-login");
   formLogin.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -11,14 +14,28 @@ async function login() {
     // Conversion du json en string pour le body de la requete
     const chargeUtile = JSON.stringify(logData);
     // le controle des champs ici est temporaire
-    if (email || password === "") {
+    // .value sinon la condition est toujours true
+    // le controle ne marche plus 19/03/24
+    if (email.value || password.value === "") {
+      console.log(email);
+      console.log(password);
       console.log("champ vide");
     } else {
-      fetch("http://localhost:5678/api/users/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: chargeUtile,
-      });
+      async function postLoginRequest() {
+        const loginRequestResponse = await fetch(
+          "http://localhost:5678/api/users/login",
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: chargeUtile,
+          }
+        );
+        //console.log(loginRequestResponse);
+        if (loginRequestResponse.ok) {
+          document.location.href = "index.html";
+        }
+      }
+      postLoginRequest();
     }
   });
 }
@@ -27,3 +44,8 @@ login();
 
 // Faire une fonction de validation de l'email et une autre pour le mdp
 // si l'une ET l'autre revoie true alors on fetch l'Api
+
+// Les logs sont :
+//email: sophie.bluel@test.tld
+//password: S0phie
+// authorizer avant sur l'api ... Si la réponses est ok alors on redirige
